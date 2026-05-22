5adfb1308c2256f3f01a534b9b327d36383c06a1
JoniMeichtry11 <joni.meichtry@gmail.com>
2026-05-22 10:25:31 -0300

    chore: dejar de rastrear environment.ts

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: (window as any)['NG_APP_SUPABASE_URL'],
-  supabaseAnonKey: (window as any)['NG_APP_SUPABASE_ANON_KEY'],
-  geminiApiKey: (window as any)['NG_APP_GEMINI_API_KEY'],
-};
